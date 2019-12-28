c4c607a9dd02ef7f3ca0fe7a18495f67aeb595d4
Carl Enlund <carl.enlund@gmail.com>
2019-12-29 00:28:13 +0100

    Use HTTPS if possible

--- index.js
+++ index.js
@@ -18,6 +18,17 @@ app.use(featurePolicy({
   },
 }));
 
+// https://stackoverflow.com/a/31144924
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
 server.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
