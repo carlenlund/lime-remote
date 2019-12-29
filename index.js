4eaeba20ef3bc05767ad2ffb14a10afdea003bf2
Carl Enlund <carl.enlund@gmail.com>
2019-12-29 02:57:04 +0100

    Server and client

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const io = require('socket.io')(server);
 
 const port = process.env.PORT || 3000;
 
+let servers = {};
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -34,5 +36,90 @@ server.listen(port, () => {
 });
 
 io.on('connection', socket => {
-  console.log(`New connection: ${socket.id}`);
+  // Client
+
+  socket.on('connectToServer', id => {
+    connectToServer(socket, id);
+  });
+
+  socket.on('moveRemote', (x, y, z) => {
+    moveRemote(socket, x, y, z);
+  });
+
+  // Server
+
+  socket.on('createServer', () => {
+    createServer(socket);
+  });
+
+  socket.on('disconnect', () => {
+    disconnectServer(socket);
+  });
 });
+
+//
+// Client code
+//
+
+function connectToServer(socket, serverId) {
+  if (!(serverId in servers)) {
+    socket.emit('invalidServerId');
+    return;
+  }
+
+  let server = servers[serverId];
+  server.clientSocket = socket;
+  server.clientSocket.emit('connectedToServer');
+  server.socket.emit('connectedToClient');
+}
+
+function moveRemote(socket, x, y, z) {
+  let server = getClientServer(socket);
+  console.log(server);
+  if (server) {
+    server.socket.emit('moveRemote', x, y, z);
+  }
+}
+
+//
+// Server code
+//
+
+function createServer(socket) {
+  let serverId = getServerId(socket);
+  servers[serverId] = {
+    socket: socket,
+    clientSocket: null,
+  };
+  socket.emit('serverCreated', serverId);
+}
+
+function disconnectServer(socket) {
+  let serverId = getServerId(socket);
+  if (!(serverId in servers)) {
+    return;
+  }
+  let server = servers[serverId];
+  if (server.clientSocket) {
+    server.clientSocket.emit('serverDisconnected');
+  }
+  delete servers[serverId];
+}
+
+//
+// Shared code
+//
+
+function getServerId(socket) {
+  return socket.id.substr(0, 5);
+}
+
+function getClientServer(socket) {
+  for (let serverId in servers) {
+    let server = servers[serverId];
+    if (server.clientSocket && server.clientSocket.id === socket.id) {
+      return server;
+    }
+  }
+  return null;
+}
--- public/main.js
+++ public/main.js
@@ -3,26 +3,58 @@ const socket = io();
 let debugElement = document.querySelector('#debug');
 let logElement = document.querySelector('#log');
 
-main();
+let startElement = document.querySelector('#start');
 
-function main() {
+let clientButtonElement = document.querySelector('#client-button');
+clientButtonElement.addEventListener('click', function() {
+  initClient();
+  startElement.style.display = 'none';
+  clientElement.style.display = 'block';
+});
+let clientElement = document.querySelector('#client');
+clientElement.style.display = 'none';
+let clientServerIdElement = document.querySelector('#client-server-id');
+let clientConnectButtonElement =
+    document.querySelector('#client-connect-button');
+clientConnectButtonElement.addEventListener('click', function() {
+  connectToServer(clientServerIdElement.value);
+});
+
+let serverButtonElement = document.querySelector('#server-button');
+let serverIdElement = document.querySelector('#server-id');
+serverButtonElement.addEventListener('click', function() {
+  initServer();
+  startElement.style.display = 'none';
+  serverElement.style.display = 'block';
+});
+let serverElement = document.querySelector('#server');
+serverElement.style.display = 'none';
+
+//
+// Client code
+//
+
+let connectedToServer = false;
+let clientServerId = null;
+
+function initClient() {
   if ('LinearAccelerationSensor' in window) {
-    navigator.permissions.query({ name: 'accelerometer' }).then(result => {
+    navigator.permissions.query({name: 'accelerometer'}).then(result => {
       if (result.state != 'granted') {
         log('Sorry, we\'re not allowed to access sensors on your device..');
         return;
       }
-      start();
+      startClient();
     }).catch(err => {
       log('Integration with Permissions API is not enabled, still try to start');
-      start();
+      startClient();
     });
   } else {
     log('Your browser doesn\'t support sensors.');
   }
 }
 
-function start() {
+function startClient() {
   let options = {
     frequency: 60,
   };
@@ -36,6 +68,8 @@ function start() {
   sensor.addEventListener('reading', e => {
     debugElement.innerHTML =
         `x: ${sensor.x}\ny: ${sensor.y}\nz: ${sensor.z}`;
+
+    socket.emit('moveRemote', sensor.x, sensor.y, sensor.z);
   });
 
   sensor.addEventListener('error', e => {
@@ -44,6 +78,52 @@ function start() {
 
   sensor.start();
 }
+
+function connectToServer(id) {
+  clientServerId = id;
+  socket.emit('connectToServer', id);
+}
+
+socket.on('connectedToServer', () => {
+  clientServerIdElement.innerHTML = clientServerId;
+  log('Connected to server');
+});
+
+socket.on('invalidServerId', () => {
+  log('Invalid server id');
+});
+
+socket.on('serverDisconnected', () => {
+  log('Disconnected from server');
+});
+
+//
+// Server code
+//
+
+function initServer() {
+  socket.emit('createServer');
+}
+
+socket.on('serverCreated', (id) => {
+  serverIdElement.innerHTML = `Server id: ${id}`;
+});
+
+socket.on('connectedToClient', () => {
+  log('Connected to client');
+});
+
+socket.on('moveRemote', (x, y, z) => {
+  let scale = 1000;
+  x = Math.round(x * scale);
+  y = Math.round(y * scale);
+  z = Math.round(z * scale);
+  debugElement.innerHTML = `x: ${x}\ny: ${y}\nz: ${z}`;
+});
+
+//
+// Shared code
+//
 
 function log() {
   logElement.innerHTML += Array.from(arguments).join(' ') + '\n';
